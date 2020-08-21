67306746c0c24e7859b92db41e66543cfd6da043
Charlie Cumber <charlie.cumber@softwire.com>
2020-08-21 16:03:45 +0100

    Addition of last editDateTime attribute of to do item

--- todo_item.py
+++ todo_item.py
@@ -1,13 +1,16 @@
+from dateutil.parser import parse
+
 class Item:
 
-    def __init__(self, id, name, status = 'To Do'):
+    def __init__(self, id, name, editDatetime, status = 'To Do'):
         self.id = id
         self.name = name
         self.status = status
+        self.editDatetime = editDatetime
 
     @classmethod
     def fromTrelloCard(cls, card, list):
-        return cls(card['id'], card['name'], list['name'])
+        return cls(card['id'], card['name'], parse(card['dateLastActivity']), list['name'])
 
     def reset(self):
         self.status = 'To Do'
