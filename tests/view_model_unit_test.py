086f1653389f0ce75f5dccb7ccf5d83da40fc3a0
CharlieCumber <charlie.cumber@softwire.com>
2021-05-27 12:56:34 +0100

    Tasks stored in a single collection with a status attribute

--- tests/view_model_unit_test.py
+++ tests/view_model_unit_test.py
@@ -8,22 +8,22 @@ from unittest import mock
 @pytest.fixture
 def test_items():
     test_items = []
-    test_items.append(Item.fromMongoDb({'_id': '1',  'name': 'Item 1',  'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}, 'To Do'))
-    test_items.append(Item.fromMongoDb({'_id': '2',  'name': 'Item 2',  'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}, 'To Do'))
-    test_items.append(Item.fromMongoDb({'_id': '3',  'name': 'Item 3',  'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}, 'To Do'))
-    test_items.append(Item.fromMongoDb({'_id': '4',  'name': 'Item 4',  'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}, 'To Do'))
-    test_items.append(Item.fromMongoDb({'_id': '5',  'name': 'Item 5',  'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}, 'Doing'))
-    test_items.append(Item.fromMongoDb({'_id': '6',  'name': 'Item 6',  'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}, 'Doing'))
-    test_items.append(Item.fromMongoDb({'_id': '7',  'name': 'Item 7',  'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}, 'Doing'))
-    test_items.append(Item.fromMongoDb({'_id': '8',  'name': 'Item 8',  'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}, 'Doing'))
-    test_items.append(Item.fromMongoDb({'_id': '9',  'name': 'Item 9',  'dateLastActivity': parse('2020-08-07 10:35:00 UTC')}, 'Done'))
-    test_items.append(Item.fromMongoDb({'_id': '10', 'name': 'Item 10', 'dateLastActivity': parse('2020-08-07 10:35:00 UTC')}, 'Done'))
-    test_items.append(Item.fromMongoDb({'_id': '11', 'name': 'Item 11', 'dateLastActivity': parse('2020-08-07 10:35:00 UTC')}, 'Done'))
-    test_items.append(Item.fromMongoDb({'_id': '12', 'name': 'Item 12', 'dateLastActivity': parse('2020-08-07 10:35:00 UTC')}, 'Done'))
-    test_items.append(Item.fromMongoDb({'_id': '13', 'name': 'Item 13', 'dateLastActivity': parse('2020-08-03 10:35:00 UTC')}, 'Done'))
-    test_items.append(Item.fromMongoDb({'_id': '14', 'name': 'Item 14', 'dateLastActivity': parse('2020-08-03 10:35:00 UTC')}, 'Done'))
-    test_items.append(Item.fromMongoDb({'_id': '15', 'name': 'Item 15', 'dateLastActivity': parse('2020-08-03 11:35:00 UTC')}, 'Done'))
-    test_items.append(Item.fromMongoDb({'_id': '16', 'name': 'Item 16', 'dateLastActivity': parse('2020-08-03 11:35:00 UTC')}, 'Done'))
+    test_items.append(Item.fromMongoDb({'_id': '1',  'name': 'Item 1',  'status': 'To Do', 'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '2',  'name': 'Item 2',  'status': 'To Do', 'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '3',  'name': 'Item 3',  'status': 'To Do', 'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '4',  'name': 'Item 4',  'status': 'To Do', 'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '5',  'name': 'Item 5',  'status': 'Doing', 'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '6',  'name': 'Item 6',  'status': 'Doing', 'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '7',  'name': 'Item 7',  'status': 'Doing', 'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '8',  'name': 'Item 8',  'status': 'Doing', 'dateLastActivity': parse('2020-08-04 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '9',  'name': 'Item 9',  'status': 'Done',  'dateLastActivity': parse('2020-08-07 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '10', 'name': 'Item 10', 'status': 'Done',  'dateLastActivity': parse('2020-08-07 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '11', 'name': 'Item 11', 'status': 'Done',  'dateLastActivity': parse('2020-08-07 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '12', 'name': 'Item 12', 'status': 'Done',  'dateLastActivity': parse('2020-08-07 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '13', 'name': 'Item 13', 'status': 'Done',  'dateLastActivity': parse('2020-08-03 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '14', 'name': 'Item 14', 'status': 'Done',  'dateLastActivity': parse('2020-08-03 10:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '15', 'name': 'Item 15', 'status': 'Done',  'dateLastActivity': parse('2020-08-03 11:35:00 UTC')}))
+    test_items.append(Item.fromMongoDb({'_id': '16', 'name': 'Item 16', 'status': 'Done',  'dateLastActivity': parse('2020-08-03 11:35:00 UTC')}))
     return test_items
 
 class TestFilters:
--- todo_app/data/mongodb.py
+++ todo_app/data/mongodb.py
@@ -3,54 +3,38 @@ from todo_app.data.todo_item import Item
 import pymongo
 from datetime import datetime
 from bson.objectid import ObjectId
-from pprint import pprint
 
 class MongoDB:
     def __init__(self):
         self.client = pymongo.MongoClient(f"mongodb+srv://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@cluster0.illz4.mongodb.net/{os.getenv('MONGODB_DATABASE_NAME')}?retryWrites=true&w=majority")
         self.db = self.client.get_default_database()
+        self.collection = self.db['Tasks']
 
     def delete_current_database(self):
         self.client.drop_database(self.db)
 
-    def get_collection(self, name):
-        return self.db[f'{name}'].find()
-
     def get_items(self):
-        items = []
-        for item in self.get_collection("To Do"):
-            items.append(Item.fromMongoDb(item, "To Do"))
-        for item in self.get_collection("Doing"):
-            items.append(Item.fromMongoDb(item, "Doing"))
-        for item in self.get_collection("Done"):
-            items.append(Item.fromMongoDb(item, "Done"))
-        return items
-
-    def get_item(self, id):
-        items = self.get_items()
-        return next((item for item in items if item.id == ObjectId(id)), None)
+        return [Item.fromMongoDb(item) for item in self.collection.find()]
 
     def add_item(self, name):
         now = datetime.utcnow()
-        item = {"name": name, "dateLastActivity": now}
-        return Item(self.db['To Do'].insert_one(item).inserted_id, name, now)
-
-    def start_item(self, id):
-        return self.move_item_to_collection(id, "To Do", "Doing")
-
-    def complete_item(self, id):
-        return self.move_item_to_collection(id, "Doing", "Done")
-
-    def uncomplete_item(self, id):
-        return self.move_item_to_collection(id, "Done", "To Do")
-
-    def delete_item(self, id):
-        item = self.get_item(id)
-        self.db[f'{item.status}'].remove({ '_id': item.id })
+        item = {"name": name, "status": "To Do", "dateLastActivity": now}
+        self.collection.insert_one(item)
         return
 
-    def move_item_to_collection(self, id, current_collection, new_collection):
-        item = self.db[f'{current_collection}'].find_one_and_delete({"_id": ObjectId(id)})
-        item['dateLastActivity'] = datetime.utcnow()
-        self.db[f'{new_collection}'].insert_one(item)
+    def start_item(self, id):
+        return self.set_item_status(id, "Doing")
+
+    def complete_item(self, id):
+        return self.set_item_status(id, "Done")
+
+    def uncomplete_item(self, id):
+        return self.set_item_status(id, "To Do")
+
+    def delete_item(self, id):
+        self.collection.delete_one({ '_id': ObjectId(id) })
+        return
+    
+    def set_item_status(self, id, status):
+        self.collection.update_one({"_id": ObjectId(id)}, {"$set": {"status": status}})
         return
